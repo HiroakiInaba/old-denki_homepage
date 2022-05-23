766cd8b5f1f7fb8b983a9d23121845c4bd4b1456
hiropen <20s3056@stu.meisei-u.ac.jp>
2022-05-23 13:48:08 +0900

    フッターを追加しました

--- Scripts/header_or_footer_gen.js
+++ Scripts/header_or_footer_gen.js
@@ -14,7 +14,8 @@ function header(){
 		backpass = "./";
 	}
 	// alert(backpass);
-	document.getElementById("header").innerHTML = '<header>'+
+	document.getElementById("header").insertAdjacentHTML('afterbegin',
+'<header>'+
 '	<div class="header_1">'+
 `		<img src="${backpass}photos/header_.png" alt="[Get Picture]"} class="header">`+
 '		<ul class="ddmenu">'+
@@ -49,7 +50,7 @@ function header(){
 '			</li>'+
 '		</ul>'+
 '	</div>'+
-'</header>'
+'</header>');
 }
 
 function footer(){
@@ -66,41 +67,25 @@ function footer(){
 		backpass = "./";
 	}
 	// alert(backpass);
-	document.getElementById("footer").innerHTML = '<footer>'+
-'	<div class="header_1">'+
-`		<img src="${backpass}photos/header_.png"} class="header">`+
-'		<ul class="ddmenu">'+
-`			<li><a href="${backpass}index.html" target="_self">HOME</a></li>`+
-'			<li><a href="#" target="_self">作品一覧</a>'+
-'				<ul type="sqare">'+
-`					<li><a href="${pass}game.html" target="_self">ゲーム作品</a></li>`+
-`					<li><a href="${pass}music.html" target="_self">メディア作品</a></li>`+
-`					<li><a href="${pass}else.html" target="_self">その他作品</a></li>`+
-'				</ul>'+
-'			</li>'+
-'			<li><a href="#" target="_self">未定</a>'+
-'				<ul type="sqare">'+
-'					<li><a href="#">ハードウェア</a></li>'+
-'					<li><a href="#">ソフトウェア</a></li>'+
-'					<li><a href="#">ウェブサービス</a></li>'+
-'				</ul>'+
-'			</li>'+
-'			<li><a href="#" target="_self">未定</a>'+
-'				<ul type="sqare">'+
-'					<li><a href="#">会社概要</a></li>'+
-'					<li><a href="#">社長挨拶</a></li>'+
-'					<li><a href="#">沿革</a></li>'+
-'					<li><a href="#">所在地</a></li>'+
-'				</ul>'+
-'			</li>'+
-'			<li><a href="#">お問い合わせ</a>'+
-'				<ul type="sqare">'+
-'					<li><a href="#">よくあるご質問</a></li>'+
-'					<li><a href="#">お問い合わせ先</a></li>'+
-'				</ul>'+
-'			</li>'+
+	document.getElementById("footer").insertAdjacentHTML('afterbegin',
+'<footer>'+
+'	<div class="footer_1">'+
+'		<ul class="list">'+
+`			<li><a href="${backpass}index.html" target="_self">・トップページ</a></li>`+
+`			<li><a href="#" target="_self">・サイトマップ</a></li>`+
+`			<li><a href="#" target="_self">・お問い合わせ</a></li>`+
+`			<li><p>SNS</p></li>`+
 '		</ul>'+
+`		<img src="${backpass}photos/download.jpg"} class="footer_">`+
+'		<a href="https://twitter.com/meisei_denkibu" target="_blank">'+
+`			<img src="${backpass}photos/twitter_logo.png"} class="tw">`+
+'		</a>'+
+'		<a href="https://www.youtube.com/channel/UC8mW4b5AgJsmDdX_oxM6t-w" target="_blank">'+
+`			<img src="${backpass}photos/youtube.png"} class="you">`+
+'		</a>'+
+'		<p class = "op">〒191-8506 東京都日野市程久保2-1-1 &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Gmail:meisei.denkibu@gmail.com<br>Ponte棟	325号室&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;製作者: 玉村雄大/根本俊佑/稲葉浩晃'+
 '	</div>'+
-'</footer>'
+'</footer>');
 }
 header();
+footer();
